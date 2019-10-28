f482d174a2301a551b6b3368e2fc85f80f7f2348
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-10-28 21:09:07 +0100

    added usertable components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/layouts/Navbar';
 import Form from './components/userForm';
+import userList from './components/userList';
 import { BrowserRouter as Router,Route} from 'react-router-dom';
 
 function App() {
@@ -9,13 +10,8 @@ function App() {
     <React.Fragment>
     <Router>
     <Navbar />
-    <Route exact path="/">
-    <h1 className="primary mb-30" style={{textAlign:"center"}}>Create a User</h1>
-    <Form />
-    </Route>
-    <Route path="/users">
-
-    </Route>
+    <Route exact path="/" component={Form}></Route>
+    <Route path="/users" component={userList}></Route>
     </Router>
     </React.Fragment>
   );
