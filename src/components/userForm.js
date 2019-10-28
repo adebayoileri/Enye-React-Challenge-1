d63d11dd520b604c68f40e2d6f0be0400478a890
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-10-28 21:09:47 +0100

    UserTable UI component

--- src/components/userForm.js
+++ src/components/userForm.js
@@ -5,23 +5,59 @@ import "react-datepicker/dist/react-datepicker.css";
 import '../App.css'
 
 export class Form extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      firstName:'',
+      lastName:'',
+      hobbies:'',
+      age:'',
+      gender:'',
+      dob:new Date()
+    }
+  }
+    onChange=(e)=>{
+      this.setState({
+        [e.target.id]: e.target.value
+      });
+    }
+
+    onChangeDate=(date)=>{ 
+      this.setState({
+          dob: date
+      });
+  }
+    onSubmit=(e)=>{
+      e.preventDefault();
+        const user={
+          firstName: this.state.firstName,
+          lastName: this.state.lastName,
+          hobbies:this.state.hobbies,
+          age:this.state.age,
+          gender:this.state.gender,
+          dob:this.state.dob
+        }
+        console.log(user)
+    }
     render() {
         return (
+          <React.Fragment>
+          <h1 className="primary mb-30" style={{textAlign:"center"}}>Create a User</h1>
             <div className="container user-board">
-    <form>
+    <form onSubmit={this.onSubmit}>
     <div className="form-group">
     <label htmlFor="Firstname">First Name:</label>
-    <input type="text" className="form-control" id="firstName" value={this.state}  placeholder="Enter Firstname" required/>
+    <input type="text" className="form-control" id="firstName" value={this.state.firstName} onChange={this.onChange}  placeholder="Enter Firstname" required/>
     </div>
 
     <div className="form-group">
     <label htmlFor="Lastname">Last Name:</label>
-    <input type="text" className="form-control" id="lastName" value={this.state}  placeholder="Enter Lastname" required/>
+    <input type="text" className="form-control" id="lastName" value={this.state.lastName} onChange={this.onChange}  placeholder="Enter Lastname" required/>
     </div>
 
     <div className="form-group">
     <label htmlFor="Hobby">Hobbies:</label>
-    <input type="text" className="form-control" id="hobbies" value={this.state}  placeholder="Enter Hobbies" required/>
+    <input type="text" className="form-control" id="hobbies" value={this.state.hobbies} onChange={this.onChange}  placeholder="Enter Hobbies" required/>
     </div>
 
     <div className="form-group">
@@ -30,14 +66,14 @@ export class Form extends Component {
     <div className="input-group-prepend">
       <span className="input-group-text">Yr(s)</span>
     </div>
-    <input type="number" className="form-control" id="age" placeholder="Enter Age" required/>
+    <input type="number" className="form-control" id="age" value={this.state.age} onChange={this.onChange}  placeholder="Enter Age" required/>
     </div>
     </div>
 
 
     <div className="form-group">
-    <label htmlFor="category">Gender:</label>
-    <select className="form-control" id="gender">
+    <label htmlFor="gender">Gender:</label>
+    <select className="form-control" id="gender"  value={this.state.gender} onChange={this.onChange}>
       <option>Male</option>
       <option>Female</option>
     </select>
@@ -45,13 +81,14 @@ export class Form extends Component {
 
          <label htmlFor="date">Date Of Birth:</label>
          <div className="form-group">
-        <DatePicker/>
+        <DatePicker selected={this.state.dob} onChange={this.onChangeDate}/>
         </div>
             <div className="form-group">
      <input type="submit" value="Add User" className="btn btn-primary"/>
      </div>
     </form>
         </div>
+        </React.Fragment>
         )
     }
 }
--- src/components/userList.js
+++ src/components/userList.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React ,{Component} from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function userList() {
+class userList extends Component{
+    render(){
     return (
-        <div>
-            
+        <React.Fragment>
+            <div className="container">
+            <h1 className="text-center mt-40">List Of All Users</h1>
+            <table className="table">
+            <thead className="thead-light">
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Gender</th>
+                        <th>Hobbies</th>
+                        <th>Age</th>
+                        <th>Date Of Birth</th>
+                        <th>Actions</th>
+                    </tr>
+            </thead>
+                <tbody>
+                {}
+                </tbody>
+            </table>
+            <Link to="/"><button type="button" className="btn btn-dark">Create new user +</button></Link>
         </div>
+        </React.Fragment>
     )
+    }
 }
+
+export default userList;
